67dbd86ab579b52fc3f2810682418485ee419a9a
Rafael Bardini <rafael@rbardini.com>
2021-11-10 22:59:33 +0100

    fix(date): move dates below titles

--- tap-snapshots/test/render.js.test.cjs
+++ tap-snapshots/test/render.js.test.cjs
@@ -204,24 +204,6 @@ blockquote > * + * {
   gap: 1em;
 }
 
-.spaced-list {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.spaced-list::before {
-  border-bottom: 1px solid var(--color-muted);
-  content: '';
-  flex-grow: 1;
-  margin: 0 1em;
-  order: 1;
-}
-
-.spaced-list > :nth-child(2) {
-  order: 2;
-}
-
 .tag-list {
   display: flex;
   flex-wrap: wrap;
@@ -311,14 +293,12 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4>CEO/President</h4>
-                  <span>
-                    <time datetime="2013-12-01">Dec 2013</time> –
-                    <time datetime="2014-12-01">Dec 2014</time>
-                  </span>
-                </div>
+                <h4>CEO/President</h4>
                 <strong><a href="http://piedpiper.example.com">Pied Piper</a></strong>
+                <div>
+                  <time datetime="2013-12-01">Dec 2013</time> –
+                  <time datetime="2014-12-01">Dec 2014</time>
+                </div>
               </header>
                 <p>Pied Piper is a multi-platform technology based on a proprietary universal compression algorithm that has consistently fielded high Weisman Scores™ that are not merely competitive, but approach the theoretical limit of lossless compression.</p>
                 <ul>
@@ -334,14 +314,12 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4>Teacher</h4>
-                  <span>
-                    <time datetime="2012-01-01">Jan 2012</time> –
-                    <time datetime="2013-01-01">Jan 2013</time>
-                  </span>
-                </div>
+                <h4>Teacher</h4>
                 <strong><a href="http://coderdojo.example.com/">CoderDojo</a></strong>
+                <div>
+                  <time datetime="2012-01-01">Jan 2012</time> –
+                  <time datetime="2013-01-01">Jan 2013</time>
+                </div>
               </header>
                 <p>Global movement of free coding clubs for young people.</p>
                 <ul>
@@ -355,14 +333,12 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4><a href="https://www.ou.edu/">University of Oklahoma</a></h4>
-                  <span>
-                    <time datetime="2011-06-01">Jun 2011</time> –
-                    <time datetime="2014-01-01">Jan 2014</time>
-                  </span>
-                </div>
+                <h4><a href="https://www.ou.edu/">University of Oklahoma</a></h4>
                   <strong>Information Technology</strong>
+                <div>
+                  <time datetime="2011-06-01">Jun 2011</time> –
+                  <time datetime="2014-01-01">Jan 2014</time>
+                </div>
               </header>
                 <p>Bachelor</p>
                 <h5>Courses</h5>
@@ -378,17 +354,15 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4><a href="missdirection.example.com">Miss Direction</a></h4>
-                  <span>
-                    <time datetime="2016-08-24">Aug 2016</time> –
-                    <time datetime="2016-08-24">Aug 2016</time>
-                  </span>
-                </div>
-                <span>
+                <h4><a href="missdirection.example.com">Miss Direction</a></h4>
+                <div>
                     <strong>Team lead and Designer</strong>
                     at <strong>Smoogle</strong>
-                </span>
+                </div>
+                <div>
+                  <time datetime="2016-08-24">Aug 2016</time> –
+                  <time datetime="2016-08-24">Aug 2016</time>
+                </div>
               </header>
                 <p>A mapping engine that misguides you</p>
                 <ul>
@@ -404,13 +378,11 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4>Digital Compression Pioneer Award</h4>
-                    <time datetime="2014-11-01">Nov 2014</time>
-                </div>
-                  <span>
+                <h4>Digital Compression Pioneer Award</h4>
+                  <div>
                     Awarded by <strong>Techcrunch</strong>
-                  </span>
+                  </div>
+                  <time datetime="2014-11-01">Nov 2014</time>
               </header>
                 <p>There is no spoon.</p>
             </article>
@@ -421,13 +393,11 @@ blockquote > * + * {
         <div class="stack">
             <article>
               <header>
-                <div class="spaced-list">
-                  <h4><a href="http://en.wikipedia.org/wiki/Silicon_Valley_(TV_series)">Video compression for 3d media</a></h4>
-                    <time datetime="2014-10-01">Oct 2014</time>
-                </div>
-                  <span>
+                <h4><a href="http://en.wikipedia.org/wiki/Silicon_Valley_(TV_series)">Video compression for 3d media</a></h4>
+                  <div>
                     Published by <strong>Hooli</strong>
-                  </span>
+                  </div>
+                  <time datetime="2014-10-01">Oct 2014</time>
               </header>
                 <p>Innovative middle-out compression algorithm that changes the way we store data.</p>
             </article>
