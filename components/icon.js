5b38c4e2fe94ffee9a9974f57f1f91894ec82ab1
Rafael Bardini <rafael@rbardini.com>
2023-02-01 01:47:10 +0100

    fix(esm): fix Feather icons import

--- components/icon.js
+++ components/icon.js
@@ -1,6 +1,7 @@
-import { icons } from 'feather-icons'
+import feather from 'feather-icons'
 
 export default function Icon(name, fallback) {
-  const ico = icons[name.toLowerCase()] || icons[fallback.toLowerCase()]
-  return ico.toSvg({ width: 16, height: 16 })
+  const icon =
+    feather.icons[name.toLowerCase()] || feather.icons[fallback.toLowerCase()]
+  return icon.toSvg({ width: 16, height: 16 })
 }
