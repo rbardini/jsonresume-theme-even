bae47af2f12a975df445b7105de4640e2300bf20
Robert Schäfer <git@roschaefer.de>
2023-08-30 13:13:10 +0200

    feat: display projects.keywords and .type
    Motivation
    ----------
    I'm using `keywords` extensively in my CV. It turns out that [resume-schema](https://github.com/jsonresume/resume-schema/blob/master/schema.json) specifies these for `projects` too. I would expect the reference implementation here to do something with it.
    
    How to test
    -----------
    1. `npm run test`
    2. Test pass
    
    *or*
    
    1. `npm run build:demo`
    2. `http-server public`
    3. Visit [localhost:8080](http://localhost:8080/)
    4. See keywords `GoogleMaps`, `Chrome Extension`, `Javascript` and type `application`

--- components/projects.js
+++ components/projects.js
@@ -3,36 +3,62 @@ import markdown from '../utils/markdown.js'
 import Duration from './duration.js'
 import Link from './link.js'
 
-const formatRoles = arr => Intl.ListFormat
-  ? new Intl.ListFormat('en').format(arr)
-  : arr.join(', ')
+const formatRoles = arr =>
+  Intl.ListFormat ? new Intl.ListFormat('en').format(arr) : arr.join(', ')
 
 export default function Projects(projects = []) {
-  return projects.length > 0 && html`
-    <section id="projects">
-      <h3>Projects</h3>
-      <div class="stack">
-      ${projects.map(({ description, entity, highlights = [], name, startDate, endDate, roles = [], url }) => html`
-        <article>
-          <header>
-            <h4>${Link(url, name)}</h4>
-            <div class="meta">
-              <div>
-                ${roles.length > 0 && html`<strong>${formatRoles(roles)}</strong>`}
-                ${entity && html`at <strong>${entity}</strong>`}
-              </div>
-              <div>${Duration(startDate, endDate)}</div>
-            </div>
-          </header>
-          ${description && markdown(description)}
-          ${highlights.length > 0 && html`
-            <ul>
-              ${highlights.map(highlight => html`<li>${markdown(highlight)}</li>`)}
-            </ul>
-          `}
-        </article>
-      `)}
-      </div>
-    </section>
-  `
+  return (
+    projects.length > 0 &&
+    html`
+      <section id="projects">
+        <h3>Projects</h3>
+        <div class="stack">
+          ${projects.map(
+            ({
+              description,
+              entity,
+              type,
+              highlights = [],
+              keywords,
+              name,
+              startDate,
+              endDate,
+              roles = [],
+              url,
+            }) => html`
+              <article>
+                <header>
+                  <h4>${Link(url, name)}</h4>
+                  <div class="meta">
+                    <div>
+                      ${roles.length > 0 &&
+                      html`<strong>${formatRoles(roles)}</strong>`}
+                      ${entity && html`at <strong>${entity}</strong>`}
+                    </div>
+                    <div>${Duration(startDate, endDate)}</div>
+                    ${type && html`<div>${type}</div>`}
+                  </div>
+                </header>
+                ${description && markdown(description)}
+                ${keywords.length > 0 &&
+                html`
+                  <ul class="tag-list">
+                    ${keywords.map(keyword => html`<li>${keyword}</li>`)}
+                  </ul>
+                `}
+                ${highlights.length > 0 &&
+                html`
+                  <ul>
+                    ${highlights.map(
+                      highlight => html`<li>${markdown(highlight)}</li>`,
+                    )}
+                  </ul>
+                `}
+              </article>
+            `,
+          )}
+        </div>
+      </section>
+    `
+  )
 }
