5575556d76f5be62cf17cf117c6f8504413975b0
Rafael Bardini <rafael@rbardini.com>
2023-09-15 03:40:25 +0200

    docs(editor): replace static demo with editor

---
+++ editor.js
@@ -0,0 +1,33 @@
+import { indentWithTab } from '@codemirror/commands'
+import { json } from '@codemirror/lang-json'
+import { EditorView, keymap } from '@codemirror/view'
+import { oneDark } from '@codemirror/theme-one-dark'
+import { basicSetup } from 'codemirror'
+import { debounce } from 'debounce'
+import resume from 'resume-schema/sample.resume.json' assert { type: 'json' }
+
+import { render } from './index.js'
+import './editor.css'
+
+const preview = document.querySelector('iframe')
+const printButton = document.querySelector('button[name=print]')
+
+printButton.addEventListener('click', () => preview.contentWindow.print())
+
+const renderPreview = debounce(resume => (preview.srcdoc = render(resume)), 200)
+renderPreview(resume)
+
+new EditorView({
+  doc: JSON.stringify(resume, null, '  '),
+  extensions: [
+    basicSetup,
+    oneDark,
+    EditorView.lineWrapping,
+    EditorView.updateListener.of(
+      ({ docChanged, state }) => docChanged && renderPreview(JSON.parse(state.doc.toString())),
+    ),
+    keymap.of([indentWithTab]),
+    json(),
+  ],
+  parent: document.body,
+})
--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,33 @@
 import { defineConfig } from 'vite'
 import pkg from './package.json' assert { type: 'json' }
 
-export default defineConfig({
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: './index.js',
-      fileName: 'index',
-      formats: ['es', 'cjs', 'umd'],
-      name: 'jsonresumeThemeEven',
+export default defineConfig(({ mode }) => {
+  if (mode === 'development') {
+    return {
+      build: {
+        outDir: './public',
+        target: 'esnext',
+      },
+      publicDir: false,
+    }
+  }
+
+  return {
+    build: {
+      copyPublicDir: false,
+      lib: {
+        entry: './index.js',
+        fileName: 'index',
+        formats: ['es', 'cjs', 'umd'],
+        name: 'jsonresumeThemeEven',
+      },
+      rollupOptions: {
+        external: [...Object.keys(pkg.dependencies), /^node:.*/],
+      },
+      target: 'esnext',
+      test: {
+        clearMocks: true,
+      },
     },
-    rollupOptions: {
-      external: [...Object.keys(pkg.dependencies), /^node:.*/],
-    },
-    target: 'esnext',
-    test: {
-      clearMocks: true,
-    },
-  },
+  }
 })
