985444d5eaa3222ccfa20ff70831938e7fa10a01
Balazs Sevecsek <mountain@tari.com>
2024-12-25 19:27:23 +0100

    Fix languages.

--- components/languages.js
+++ components/languages.js
@@ -8,9 +8,9 @@ export default function Languages(languages = []) {
   return (
     languages.length > 0 &&
     html`
-      <section id="languages" class="display-unit">
+      <section id="languages">
         <h3>Languages</h3>
-        <div class="grid-list">
+        <div class="grid-list display-unit">
           ${languages.map(
             ({ fluency, language }) => html`<div>${language && html`<h4>${language}</h4>`} ${fluency}</div>`,
           )}
